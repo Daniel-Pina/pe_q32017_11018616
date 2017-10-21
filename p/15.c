d4d86f94c87f7fe3f9e23e119e5f9d8a5d30b375
Daniel-Pina <daniel.pina@outlook.com>
2017-10-21 01:40:15 +0000

    Add files via upload

--- p/15.c
+++ p/15.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
+
+/************************************ PROGRAMA NAO FUNCIONOU QUANDO MODULARIZADO ************************************/
 
 /* Função que imprime as hastes (debug) */
 void p_hastes_d(int n, int * origem, int * auxiliar, int * destino)
 {
-    int i, q;
-    q = n*sizeof(int);
-    for (i=0; i<q; i++) {
-        printf("\n%d ", origem[i]);
+    int i;
+    for (i=0; i<n; i++) {
+        printf("%d ", origem[i]);
     }
-    for (i=0; i<q; i++) {
-        printf("\n%d ", auxiliar[i]);
+    printf("\n");
+    for (i=0; i<n; i++) {
+        printf("%d ", auxiliar[i]);
     }
-    for (i=0; i<q; i++) {
-        printf("\n%d ", destino[i]);
+    printf("\n");
+    for (i=0; i<n; i++) {
+        printf("%d ", destino[i]);
     }
     printf("\n\n");
 }
@@ -21,13 +25,14 @@ void p_hastes_d(int n, int * origem, int * auxiliar, int * destino)
 /* Função que imprime a solução (debug) */
 void p_solucao_d(int n, char ** solucao)
 {
-    int i, j, movimentos;
-    movimentos = sizeof(int *)*n;
+    int i, movimentos;
+    movimentos = pow(2, n) - 1;
     for (i=0; i<movimentos-1; i++) {
         printf("%c-%c, ", solucao[i][0], solucao[i][1]);
     }
     printf("%c-%c", solucao[movimentos-1][0], solucao[movimentos-1][1]);
 }
+
 
 /*
  *
@@ -36,14 +41,13 @@ void p_solucao_d(int n, char ** solucao)
 void resolve_hanoi(int n, int * origem, int * auxiliar, int * destino, char ** solucao)
 {
     int i, mov;
-    mov = sizeof(int *)*n;
+    //mov = sizeof(int *)*n;
     for (i=0; i<mov; i++) {
 
 
 
     }
 }
-
 
 
 /* Função que inicializa as três hastes */
@@ -54,7 +58,7 @@ void hastes(int n, int * origem, int * auxiliar, int * destino)
     origem = malloc(q);
     auxiliar = malloc(q);
     destino = malloc(q);
-    for (i=0; i<q; i++) {
+    for (i=0; i<n; i++) {
         origem[i] = i+1;
         auxiliar[i] = 0;
         destino[i] = 0;
@@ -64,12 +68,55 @@ void hastes(int n, int * origem, int * auxiliar, int * destino)
 /* Função que resolve a torre de hanoi para n discos */
 void hanoi(int n, int * origem, int * auxiliar, int * destino, char ** solucao)
 {
-    int i;
-    hastes(n, origem, auxiliar, destino);
-    for (i=0; i<2; i++) solucao[i] = malloc(3*sizeof(int));
-    solucao = malloc(sizeof(int *)*n);
-    resolve_hanoi(n, origem, auxiliar, destino, solucao);
-    p_solucao_d(n, solucao);
+    int i, Q;
+    char model[2];
+    q = n*sizeof(int);
+    origem = malloc(q);
+    auxiliar = malloc(q);
+    destino = malloc(q);
+    for (i=0; i<n; i++) {
+        origem[i] = i+1;
+        auxiliar[i] = 0;
+        destino[i] = 0;
+    }
+    solucao = malloc(sizeof(model)*(pow(2, n) - 1));
+    for (i=0; i<(pow(2, n) - 1); i++) solucao[i] = malloc(2*sizeof(char));
+
+
+	char ultima_haste;
+
+    for (i=0; i<(pow(2, n) - 1); i++) {
+		if (i == 0) {
+			origem[0] = 0;
+			if (n%2 == 0) {
+				auxiliar[0] = 1;
+				ultima_haste = 'A';
+			}
+			else {
+				destino[0] = 1;
+				ultima_haste = 'D';
+			}
+		}
+		else {
+			switch (ultima_haste) {
+				case ''
+	
+
+			}
+
+
+
+		}
+	}
+    
+
+
+	int movimentos;
+    movimentos = pow(2, n) - 1;
+    for (i=0; i<movimentos-1; i++) {
+        printf("%c-%c, ", solucao[i][0], solucao[i][1]);
+    }
+    printf("%c-%c", solucao[movimentos-1][0], solucao[movimentos-1][1]);
 }
 
 int main()
@@ -83,14 +130,11 @@ int main()
     scanf("%d", &n_discos);
     hanoi(n_discos, origem, auxiliar, destino, solucao);
 
-
-    p_hastes_d(n_discos, origem, auxiliar, destino);
-
     int i;
     free(origem);
     free(auxiliar);
     free(destino);
-    for (i=0; i<2; i++) free(solucao[i]);
+    for (i=0; i<(pow(2, n_discos) - 1); i++) free(solucao[i]);
     free(solucao);
 
 	return 0;
